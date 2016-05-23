535e80a168c4e640e84e96ff9e62bb78bb1ad8a5
Adetunji Sunmonu <tunji.sunmonu@gmail.com>
2016-05-24 09:27:25 +1000

    returned the data from directions

--- app/controllers/direction.js
+++ app/controllers/direction.js
@@ -2,7 +2,7 @@ var request = require("request");
 
 var directions = function(){
 	return request.get('http://maps.googleapis.com/maps/api/directions/json?&mode=DRIVING&origin=brisbane&destination=sydney', function (error, response, body) {
-		return body.body;
+		return response.body;
 	});
 };
 
