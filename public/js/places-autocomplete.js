dcbc8e67914841ea6c77668ce4bdfd434bcdba3b
Roni Greenwood <rgrnwd@Ronis-MacBook-Pro.local>
2016-05-24 14:03:10 +1000

    found address suburb

--- public/js/places-autocomplete.js
+++ public/js/places-autocomplete.js
@@ -1,12 +1,17 @@
 google.maps.event.addDomListener(window, 'load', function () {
-    var startAddress = new google.maps.places.Autocomplete(document.getElementById('start-address'));
-    var destAddress = new google.maps.places.Autocomplete(document.getElementById('dest-address'));
+    var startAddress = new google.maps.places.Autocomplete(
+        document.getElementById('start-address'));
+
+    var destAddress = new google.maps.places.Autocomplete(
+        document.getElementById('dest-address'));
+
     google.maps.event.addListener(startAddress, 'place_changed', function () {
         var place = startAddress.getPlace();
         var address = place.formatted_address;
         var latitude = place.geometry.location.lat();
         var longitude = place.geometry.location.lng();
 
+        document.getElementById("startPostCode").value = getPostalCode(place);
         document.getElementById("startAddressLat").value = latitude;
         document.getElementById("startAddressLong").value = longitude;
     });
@@ -16,7 +21,24 @@ google.maps.event.addDomListener(window, 'load', function () {
         var latitude = place.geometry.location.lat();
         var longitude = place.geometry.location.lng();
 
+        document.getElementById("destPostCode").value = getPostalCode(place);
         document.getElementById("destAddressLat").value = latitude;
         document.getElementById("destAddressLong").value = longitude;
     });
 });
+
+function getPostalCode(place){
+    
+    var postalCode = '';
+    for (var i = 0; i < place.address_components.length; i++) 
+    {
+        var addressType = place.address_components[i].types[0];
+
+        if (addressType=='postal_code') {
+          postalCode = place.address_components[i].short_name;
+          break;
+        }
+    }
+
+    return postalCode;
+}
