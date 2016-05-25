792c26e286ac438e983bdfc1a904a1cc2d310130
minillinim <michael.imelfort@gmail.com>
2016-05-25 10:17:08 +1000

    [MI] Pre pull commit

--- app/controllers/map.js
+++ app/controllers/map.js
@@ -1,9 +1,10 @@
 var request = require("request");
 var tl_api = require('../helpers/translinkapi');
 
-var directions = function(startAddressLat,startAddressLong, destAddressLat, destAddressLong){
+var directions = function(startAddressLat, startAddressLong, destAddressLat, destAddressLong) {
+    console.log(startAddressLat, startAddressLong, destAddressLat, destAddressLong)
 	return request.get('https://maps.googleapis.com/maps/api/directions/json?&mode=DRIVING&origin='+ startAddressLat +','+startAddressLong+'&destination='+ destAddressLat+','+destAddressLong, function (error, response, body) {
-        var start_location = tl_api.getLocation(startAddressLat,startAddressLong);
+        var start_location = tl_api.getLocation(startAddressLat, startAddressLong);
         var end_location = tl_api.getLocation(destAddressLat, destAddressLong);
 		return response.body;
 	});
