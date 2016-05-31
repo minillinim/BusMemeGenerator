918a20ade9a0d8aedf9973aa4f2d59e43bf1dc96
Roni Greenwood <rgrnwd@Ronis-MacBook-Pro.local>
2016-05-31 14:34:49 +1000

    added some meme templates, linked user meme template selection to meme

--- app/db/memeTemplate.js
+++ app/db/memeTemplate.js
@@ -5,6 +5,18 @@ exports.memeTemplates = [
     },
     {   
         "firstLine":"LOGAN CITY COUNCIL",
-        "secondLine":"NEEDS TO INVEST IN PUBLIC TRANSPORT!"
+        "secondLine":"INVEST IN BETTER BUSES!"
+    },
+    {   
+        "firstLine":"And then they wonder",
+        "secondLine":"Why everyone owns a car"
+    },
+    {   
+        "firstLine":"And this \"service\" costs",
+        "secondLine":"HOW MUCH??"
+    },
+    {   
+        "firstLine":"LOGAN CITY COUNCIL",
+        "secondLine":"INVEST IN BETTER BUSES!"
     }
 ];
--- public/js/meme-templates.js
+++ public/js/meme-templates.js
@@ -1,20 +1,27 @@
 var app = angular.module('bus-meme');
 
 app.controller('MemeController', function ($scope, MemeFactory, $anchorScroll) {
+   
     MemeFactory.getMemeTemplates().then(function(response){
+
    		$scope.memeTemplates = response.data;
+	    $scope.selectedTemplate = $scope.memeTemplates[0];
+		
     });
-    $scope.memeReady = false;
 
-	var imageContainer = document.getElementById("map-image");
-	var image = document.getElementById("img-out");
-	var canvas = document.getElementById("canvas"); 
-	var context = canvas.getContext("2d");
-
-	var topText = 'Public Transport';
-	var bottomText = 'FAIL!!!';
+	$scope.setSelectedTemplate = function (template) {
+	   	$scope.selectedTemplate = template;
+   		$scope.renderMeme();
+	};
 
 	$scope.renderMeme = function () {
+		var imageContainer = document.getElementById("map-image");
+		var image = document.getElementById("img-out");
+		var canvas = document.getElementById("canvas"); 
+		var context = canvas.getContext("2d");
+
+		var topText = $scope.selectedTemplate.firstLine;
+		var bottomText = $scope.selectedTemplate.secondLine;
 
 	    canvas.width = image.width;
 	    canvas.height = image.height;
@@ -23,17 +30,19 @@ app.controller('MemeController', function ($scope, MemeFactory, $anchorScroll) {
 	    context.fillStyle = "white";
 	    context.strokeStyle = "black";
 	    context.lineWidth = 2;
-	    writeTextOnImage(topText, canvas.width / 2, 50);
-	    writeTextOnImage(bottomText, canvas.width / 2, canvas.height - 20);
+	    writeTextOnImage(topText, canvas.width / 2, 70);
+	    writeTextOnImage(bottomText, canvas.width / 2, canvas.height - 30);
+	    // downloadLink.href = canvas.toDataURL("image/jpeg");
 	    $scope.memeReady = true;
 
 	    function writeTextOnImage (text, x, y) {
 	        var f = 36; 
 	        for (; f >= 0; f -=1) {
-	            context.font = "bold " + f + "pt Impact, Charcoal, sans-serif";
+	            context.font = "bold 42pt Impact, Charcoal, sans-serif";
 	            if (context.measureText(text).width < canvas.width - 10) {
 	                context.fillText(text, x, y);
 	                context.strokeText(text, x, y);
+
 	                break;
 	            }
 	        }
