57988b82ed43035f08630342eec7ba3f63d164d4
Roni Greenwood <rgrnwd@Ronis-MacBook-Pro.local>
2016-05-31 11:55:33 +1000

    add meme text on top of image

--- public/js/meme-templates.js
+++ public/js/meme-templates.js
@@ -1,50 +1,44 @@
 var app = angular.module('bus-meme');
 
 app.controller('MemeController', function ($scope, MemeFactory, $anchorScroll) {
-   MemeFactory.getMemeTemplates().then(function(response){
+    MemeFactory.getMemeTemplates().then(function(response){
    		$scope.memeTemplates = response.data;
-   });
+    });
+    $scope.memeReady = false;
 
-	// var e = {}, // A container for DOM elements
-	//     image = new Image(),
-	//     ctxt = null, // For canvas' 2d context
-	//     renderMeme = null, // For a function to render memes
-	//     get = function (id) {
-	//         // Short for document.getElementById()
-	//         return document.getElementById(id);
-	//     };
-	// // Get elements (by id):\
-	// e.imageContainer = get("map-image");
-	// e.topline = get("topline");
-	// e.bottomline = get("bottomline");
-	// e.canvas = get("canvas"); // canvas;
-	// // Get canvas context:
-	// ctxt = e.canvas.getContext("2d");
+	var imageContainer = document.getElementById("map-image");
+	var image = document.getElementById("img-out");
+	var canvas = document.getElementById("canvas"); 
+	var context = canvas.getContext("2d");
 
- //   	$scope.renderMeme = function () {
-	//     e.canvas.width = image.width;
-	//     e.canvas.height = image.height;
-	//     ctxt.drawImage(image, 0, 0, e.canvas.width, e.canvas.height);
-	//     ctxt.textAlign = "center";
-	//     ctxt.fillStyle = "white";
-	//     ctxt.strokeStyle = "black";
-	//     ctxt.lineWidth = 2;
-	//     writeText(e.topline.value, e.canvas.width / 2, 50);
-	//     writeText(e.bottomline.value, e.canvas.width / 2, e.canvas.height - 20);
-	//     e.downloadLink.href = e.canvas.toDataURL("image/jpeg");
+	var topText = 'Public Transport';
+	var bottomText = 'FAIL!!!';
 
-	//     var writeText = function (text, x, y) {
-	//         var f = 36; // Font size (in pt)
-	//         for (; f >= 0; f -=1) {
-	//             ctxt.font = "bold " + f + "pt Impact, Charcoal, sans-serif";
-	//             if (ctxt.measureText(text).width < e.canvas.width - 10) {
-	//                 ctxt.fillText(text, x, y);
-	//                 ctxt.strokeText(text, x, y);
-	//                 break;
-	//             }
-	//         }
-	//     };
-	// };
+	$scope.renderMeme = function () {
+
+	    canvas.width = image.width;
+	    canvas.height = image.height;
+	    context.drawImage(image, 0, 0, canvas.width, canvas.height);
+	    context.textAlign = "center";
+	    context.fillStyle = "white";
+	    context.strokeStyle = "black";
+	    context.lineWidth = 2;
+	    writeTextOnImage(topText, canvas.width / 2, 50);
+	    writeTextOnImage(bottomText, canvas.width / 2, canvas.height - 20);
+	    $scope.memeReady = true;
+
+	    function writeTextOnImage (text, x, y) {
+	        var f = 36; 
+	        for (; f >= 0; f -=1) {
+	            context.font = "bold " + f + "pt Impact, Charcoal, sans-serif";
+	            if (context.measureText(text).width < canvas.width - 10) {
+	                context.fillText(text, x, y);
+	                context.strokeText(text, x, y);
+	                break;
+	            }
+	        }
+	    };
+	};
 });
 
 app.factory('MemeFactory', ['$http', function ($http) {
