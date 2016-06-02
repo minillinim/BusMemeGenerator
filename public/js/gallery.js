70b5ed76268d60eb485a110abe8f45531a1474e6
Roni Greenwood <rgrnwd@Ronis-MacBook-Pro.local>
2016-06-02 21:16:18 +1000

    display distance/duration properly in gallery

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -11,6 +11,27 @@ app.controller('GalleryController', function ($scope, $location, MemeFactory) {
     $scope.showHomePage = function () {
         $location.path('');
     }
+
+    $scope.formatDistance = function(distance){
+        if (distance < 1000)
+            return distance + "m";
+        else{
+            var distanceInKm = distance / 1000;
+            return distanceInKm + "km";
+        }
+    }
+    $scope.formatTravelTime = function(time){
+
+        var timeInMinutes = time / (60000);
+        if (timeInMinutes < 60){
+            return timeInMinutes + " mins";
+        }
+        else{
+            var hours = (timeInMinutes / 60).toFixed(0);
+            var minutesLeft = timeInMinutes - (hours*60);
+            return hours + " hrs " + minutesLeft + " mins";
+        }
+    }
 });
 
 app.filter('transportMode', function () {
