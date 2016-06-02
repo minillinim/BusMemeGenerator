798af3bb660010d3fe51cc082ca58d1b6bb05384
Adetunji Sunmonu <tunji.sunmonu@gmail.com>
2016-06-02 16:13:39 +1000

    added filter function

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -1,13 +1,26 @@
 var app = angular.module('bus-meme');
 
 app.controller('GalleryController', function ($scope, $location, MemeFactory) {
+    $scope.images = [];
+    $scope.travelMode = 'walking';
 
     MemeFactory.getImages().then(function (response) {
         $scope.images = response.data
     });
 
-    $scope.showHomePage = function(){
+    $scope.showHomePage = function () {
         $location.path('');
     }
+});
 
+app.filter('transportMode', function () {
+    return function (images, transportMode) {
+        if (transportMode && images.length > 0) {
+            return images.filter(function (image) {
+                return image.otherMode === transportMode.toLowerCase();
+            })
+        } else {
+            return images;
+        }
+    };
 });
