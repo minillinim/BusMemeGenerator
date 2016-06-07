8a68521df5be2d8c2484a22aa6b92728d8af2c2d
Roni Greenwood <rgrnwd@Ronis-MacBook-Pro.local>
2016-06-07 17:00:22 +1000

    fix the one test

--- spec/home-spec.js
+++ spec/home-spec.js
@@ -4,7 +4,7 @@ var app = require("../app.js")
 describe("home", function(){
 
  it("should respond with hello world", function(done) {
-   request.get("http://localhost:3000/", function(error, response, body){
+   request.get("http://localhost:3000/logan/", function(error, response, body){
      expect(response.statusCode).toEqual(200);
      done();
    });
